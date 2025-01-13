868bd1093dff3f73ab8eb224ab65ea2b9263076e
Young So <youngso@YoungdeMacBook-Air.local>
2025-01-13 15:22:07 +0800

    Initial commit

---
+++ CPP/sorting.h
@@ -0,0 +1,18 @@
+// Sorting algorithms
+#ifndef SORTING_H
+#define SORTING_H
+
+// swap
+template<typename T>
+void swap(T arr[], int i, int j){
+    
+}
+
+// selectSort
+template<typename T>
+void selectSort(T arr[], int n){
+
+}
+
+
+#endif // SORTING_H
